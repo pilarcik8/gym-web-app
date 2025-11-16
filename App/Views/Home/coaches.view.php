c4215299b7090410e11020a097779e7d655e5f9f
Stick <sompilarcikmichal@gmail.com>
2025-11-16 15:15:28 +0100

    dokoncena stranka trenery, tlacidla sa mi nepacia ako su ulozene ale nechce sa mi to uz upravovat

--- App/Views/Home/coaches.view.php
+++ App/Views/Home/coaches.view.php
@@ -9,6 +9,7 @@
 
 <body>
 
+<!-- Hero -->
 <div class="hero-coaches-wrapper">
     <img src="<?= $link->asset('/images/couches-hero.png') ?>" alt="Trener joga" class="hero-img">
 
@@ -18,48 +19,58 @@
     </div>
 </div>
 
-<!-- obrázok vpravo, kosodĺžnik vľavo -->
-<section class="row-section section-left-koso" id="section1">
-    <div class="parallelogram" id="koso1">
-        <div class="parallelogram-content">
+<!-- Treneri -->
+<section class="row-section">
+    <div>
+        <button class="btn btn-primary">Rezervuj si Trénera <span class="button-text-yellow">MAREK</span></button>
+    </div>
+    <div class="slashed-rectangle-right">
+        <div class="slashed-rectangle-content">
             <div>
-            <h5 class="koso-name">MAREK</h5>
-            <p class="koso-popisok">silový tréning a kondícia</p>
-
+            <h5 class="couach-name">MAREK</h5>
+            <p class="couach-short-info">SILOVÝ TRÉNING A KONDÍCIA</p>
             Marek je certifikovaný tréner so zameraním na silový a funkčný tréning. Pomôže vám vybudovať svaly, zlepšiť výkon a naučí vás správnu techniku cvičenia.
             </div>
+        </div>
+    </div>
+
+    <img src="<?= $link->asset('/images/coach-1.png') ?>" alt="trener1" class="section-image-right">
+</section>
+
+<section class="row-section">
+    <img src="<?= $link->asset('/images/coach-2.png') ?>" alt="trener2" class="section-image-left">
+
+    <div class="slashed-rectangle-left">
+        <div class="slashed-rectangle-content">
             <div>
-                <button>
-                    Rezervuj si
-                </button>
+                <h5 class="couach-name">LUCIA</h5>
+                <p class="couach-short-info">FITENESS A ZDRAVÝ ŽIVOTNÝ ŠTÝL</p>
+                Lucia je energická trénerka, ktorá kombinuje silový tréning s prvkami mobility a jógy. Pomôže vám cítiť sa lepšie, silnejšie a sebavedomejšie každý deň.
             </div>
-
         </div>
     </div>
 
-    <img src="<?= $link->asset('/images/coach-1.png') ?>" alt="trener1" class="section-image" id="img1">
-</section>
-
-<!-- obrázok vľavo, kosodĺžnik vpravo -->
-<section class="row-section section-right-koso" id="section2">
-    <img src="<?= $link->asset('/images/coach-1.png') ?>" alt="trener1" class="section-image" id="img2">
-
-    <div class="parallelogram" id="koso2">
-        <div class="parallelogram-content">
-            Toto je text v kosodĺžniku (vpravo)
-        </div>
+    <div>
+        <button class="btn btn-primary">Rezervuj si Trénera <span class="button-text-yellow">LUCIA</span></button>
     </div>
 </section>
 
-<!-- obrázok vpravo, kosodĺžnik vľavo -->
-<section class="row-section section-left-koso" id="section1">
-    <div class="parallelogram" id="koso1">
-        <div class="parallelogram-content">
-            Toto je text v kosodĺžniku (vľavo)
+<section class="row-section">
+    <div>
+        <button class="btn btn-primary">Rezervuj si Trénera <span class="button-text-yellow">PETER</span></button>
+    </div>
+
+    <div class="slashed-rectangle-right">
+        <div class="slashed-rectangle-content">
+            <div>
+                <h5 class="couach-name">MARTIN</h5>
+                <p class="couach-short-info">SILOVÝ TRÉNING A KONDÍCIA</p>
+                Martin sa zameriava na rozvoj sily, správnu techniku a dlhodobú kondíciu. Jeho tréningy sú dynamické, premyslené a prispôsobené úrovni každého klienta.
+            </div>
         </div>
     </div>
 
-    <img src="<?= $link->asset('/images/coach-1.png') ?>" alt="trener1" class="section-image" id="img1">
+    <img src="<?= $link->asset('/images/coach-3.png') ?>" alt="trener3" class="section-image-right">
 </section>
 
 </body>
