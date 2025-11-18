420a1c33c8220d04a72a54d56b14159219ac66b7
Stick <sompilarcikmichal@gmail.com>
2025-11-18 14:51:47 +0100

    dokoncena stranka permits

--- App/Views/Home/permits.view.php
+++ App/Views/Home/permits.view.php
@@ -9,4 +9,52 @@ $view->setLayout('root');
     <link rel="stylesheet" href="<?= $link->asset('/css/permits.css') ?>">
 </head>
 
-<div class="permits-hero"></div>
+<div class="permits-hero">
+    <h1>Cenník</h1>
+    <h2>Pernamentka</h2>
+
+    <div class="container">
+
+        <div class="rectengle">
+            <div class="rect-header">
+                <h3>Týždenná</h3>
+            </div>
+
+            <div class="rect-body">
+                <p>Dĺžka trvania: <span>7 dní</span></p>
+                <p>Celková cena: <span>15 €</span></p>
+                <p>Cena za deň: <span>2.14 € / deň</span></p>
+            </div>
+
+            <button class="btn btn-primary button-green" type="submit" name="submit">Kúpiť</button>
+        </div>
+
+        <div class="rectengle">
+            <div class="rect-header">
+                <h3>Mesačná</h3>
+            </div>
+
+            <div class="rect-body">
+                <p>Dĺžka trvania: <span>30 dní</span></p>
+                <p>Celková cena: <span>45 €</span></p>
+                <p>Cena za deň: <span>1.50 € / deň</span></p>
+            </div>
+
+            <button class="btn btn-primary button-green" type="submit" name="submit">Kúpiť</button>
+        </div>
+
+        <div class="rectengle">
+            <div class="rect-header">
+                <h3>Ročná</h3>
+            </div>
+
+            <div class="rect-body">
+                <p>Dĺžka trvania: <span>365 dní</span></p>
+                <p>Celková cena: <span>400 €</span></p>
+                <p>Cena za deň: <span>1.10 € / deň</span></p>
+            </div>
+
+            <button class="btn btn-primary button-green" type="submit" name="submit">Kúpiť</button>
+        </div>
+    </div>
+</div>
